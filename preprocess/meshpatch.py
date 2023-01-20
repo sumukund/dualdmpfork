a32fd0b27bde3c8d0803e6f42c4d73c99caf68da
Sudarsna.Mukund <Sudarsna.Mukund@target.com>
2023-01-20 13:38:10 -0600

    combining

--- preprocess/meshpatch.py
+++ preprocess/meshpatch.py
@@ -2,37 +2,63 @@ import numpy as np
 import pyvista as pv
 import pymeshfix
 from pymeshfix import PyTMesh
+import pymeshlab
 import argparse
 import argparse
 
 # takes in a file
-def get_parser():
+def get_parser() -> argparse:
     parser = argparse.ArgumentParser(description='create datasets(noisy mesh & smoothed mesh) from a single clean mesh')
     parser.add_argument('-i', '--input', type=str, required=True)
     parser.add_argument('-o', '--output', type=str, required=True)
     args = parser.parse_args()
-
-    for k, v in vars(args).items():
-        print('{:12s}: {}'.format(k, v))
-
     return args
 
-filename = args.input
+class MeshRepair:
+    # Fills all the holes having at least 'nbe' boundary edges. If
+    # 'refine' is true, adds inner vertices to reproduce the
+    # sampling density of the surroundings. Returns number of holes
+    # patched.  If 'nbe' is 0 (default), all the holes are patched.
+    def fillBoundaries(mfix):
+        mfix.remove_smallest_components()
+        mfix.fill_small_boundaries(nbe=1, refine=True)
+        mfix.clean(max_iters=5, inner_loops=0.5)
+        vert, faces = mfix.return_arrays()
+        mfix = pymeshfix.MeshFix(vert, faces)
+        #repairs holes and removes artifacts
+        mfix.repair()
+        mfix.plot()
 
-orig_mesh = pv.read(filename)
-#converts it to a PyTMesh object
-mfix = PyTMesh(False)
-mfix.load_file(filename)
+class PyMeshLab:
+    ms = pymeshlab.MeshSet()
+    def selfIntersections(ms):
+        ms.compute_selection_by_self_intersections_per_face()
+        ms.meshing_remove_selected_vertices()
+    def removeTVertices(ms):
+        ms.meshing_remove_t_vertices()
+    def removeDuplicates(ms):
+        ms.meshing_remove_duplicate_faces()
+        ms.meshing_remove_duplicate_vertices()
+        ms.meshing_remove_null_faces()
+    def repairManifold(ms):
+        ms.meshing_repair_non_manifold_edges(0)
+    def removeConnected(ms):
+        ms.meshing_remove_connected_component_by_diameter(0.10, True)
 
-# Fills all the holes having at least 'nbe' boundary edges. If
-# 'refine' is true, adds inner vertices to reproduce the
-# sampling density of the surroundings. Returns number of holes
-# patched.  If 'nbe' is 0 (default), all the holes are patched.
+def main(): 
+    args = get_parser()
+    print(args)
+    #mfix = pv.read(args.input)
+    #converts it to a PyTMesh object
+    mfix = PyTMesh(True)
+    mfix.load_file(args.input)
+    MeshRepair.fillBoundaries(mfix)
+    ms = pymeshlab.MeshSet()
+    ms.load_new_mesh(args.input)
+    PyMeshLab.selfIntersections(ms)
+    PyMeshLab.removeTVertices(ms)
+    ms.save_current_mesh(args.output)
+    mfix.save_file(args.output)
 
-mfix.fill_small_boundaries(nbe=1, refine=True)
-mfix.clean(max_iters=5, inner_loops=0.5)
-vert, faces = mfix.return_arrays()
-mfix = pymeshfix.MeshFix(vert, faces)
-#repairs holes and removes artifacts
-mfix.repair()
-mfix.save(args.output)
+if __name__ == "__main__":
+    main()
